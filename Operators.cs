0f05dd74196d04ad19f335d22f512854f04f5b01
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-12-06 18:46:31 -0500

    Create Operators.cs

---
+++ Operators.cs
@@ -0,0 +1,75 @@
+//Variable declaration
+
+int x, y, z;
+
+//Assignment operator
+
+x = 1;
+y = 3;   
+z = 2;
+
+//Mathematical Operators
+
+//Addition Operator
+
+x = 5 + 4;
+
+//Substraction Operator
+
+x = 5 - 4;
+
+//Multiplication Operator
+
+x = 5 * 4;
+
+//Divition Operator
+
+x = 5 / 4;
+
+//Order of Operations using parenthesis
+
+x = (y + z) * (y - z);
+
+//Other Operators
+
+//Equality Operator
+
+if ( x == y )
+{
+}
+
+//Greather than Operator
+
+if ( x > y )
+{
+}
+
+//Less than Operator
+
+if ( x < z )
+{
+}
+
+//Greater or equal Operator
+
+if ( x >= z )
+{
+}
+
+//Less or equal Operator
+
+if ( x <= z)
+{
+}
+
+//Conditional AND Operator
+
+if (( y < z ) && ( z > x ))
+{
+}
+
+//Conditional OR Operator
+
+if (( z > y ) || ( y > z ))
+{
+}
